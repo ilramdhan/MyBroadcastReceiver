685d733ed916ad5986e702dc1b71a5b979b76dc1
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-28 19:05:01 +0700

    Step 3

--- app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -3,9 +3,27 @@ package com.dicoding.mybroadcastreceiver
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.btnPermission?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
